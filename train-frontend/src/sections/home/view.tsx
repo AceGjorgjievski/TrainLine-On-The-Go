6e4cc35699192260fcce4c67b17f09aae905981f
Ace <ace.gjorgjievski6@gmail.com>
2025-07-31 22:46:54 +0200

    refactor: remove map view from home page

--- train-frontend/src/sections/home/view.tsx
+++ train-frontend/src/sections/home/view.tsx
@@ -1,11 +1,7 @@
-'use client';
-import dynamic from "next/dynamic";
 
-
-const Map = dynamic(() => import('./map-container-view'), { ssr: false });
 
 export default function HomeView() {
     return (
-        <Map/>
+        <>Home View</>
     );
 }
