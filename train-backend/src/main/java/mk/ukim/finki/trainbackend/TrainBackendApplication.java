55529baad1169e5983226a60a126af84db37efef
Ace <ace.gjorgjievski6@gmail.com>
2025-08-11 11:00:52 +0200

    feat: register PasswordEncoder as a Bean

--- train-backend/src/main/java/mk/ukim/finki/trainbackend/TrainBackendApplication.java
+++ train-backend/src/main/java/mk/ukim/finki/trainbackend/TrainBackendApplication.java
@@ -2,6 +2,9 @@ package mk.ukim.finki.trainbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TrainBackendApplication {
@@ -10,4 +13,8 @@ public class TrainBackendApplication {
         SpringApplication.run(TrainBackendApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
