854ad89658ccb149596aca19828028689ad6c063
Ace <ace.gjorgjievski6@gmail.com>
2025-09-20 00:30:17 +0200

    style: update button dashboard collapse

--- train-frontend/src/layouts/dashboard/index.tsx
+++ train-frontend/src/layouts/dashboard/index.tsx
@@ -1,32 +1,32 @@
 "use client";
 
-import { Header } from "@/components/header";
 import {
   Box,
   colors,
   CssBaseline,
-  Divider,
   Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Toolbar,
+  Tooltip,
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import ScheduleIcon from "@mui/icons-material/Schedule";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
-
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import { usePathname, useRouter } from "../../../i18n/routing";
 import { paths } from "@/routes/paths";
+import { useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const drawerWidth = 240;
 const drawerItems = [
   {
     label: "Home",
@@ -53,7 +53,8 @@ const drawerItems = [
 export default function DashBoardLayout({ children }: Props) {
   const router = useRouter();
   const pathName = usePathname();
-
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const drawerWidth = isCollapsed ? 82 : 240;
   const handleNavigate = (path: string) => {
     router.push(path);
   };
@@ -69,58 +70,100 @@ export default function DashBoardLayout({ children }: Props) {
             flexShrink: 0,
             [`& .MuiDrawer-paper`]: {
               width: drawerWidth,
+              top: "87px",
+              height: "calc(100% - 87px)",
+              overflowX: "hidden",
+              transition: "width 0.3s ease",
               boxSizing: "border-box",
+              boxShadow: 10,
             },
+            zIndex: 1,
           }}
         >
-          <Toolbar>Logo</Toolbar>
-          <Divider />
+          {/* <Divider /> */}
           <Box sx={{ overflow: "auto" }}>
             <List>
               {drawerItems.map(({ label, path, icon }, index) => {
                 const selected = pathName === path;
                 return (
-                  <ListItem key={index} disablePadding>
-                    <ListItemButton
-                      selected={selected}
-                      onClick={() => handleNavigate(path)}
+                  <Tooltip
+                    title={isCollapsed ? label : ""}
+                    placement="right"
+                    key={index}
+                    arrow
+                  >
+                    <ListItem
+                      disablePadding
                       sx={{
-                        marginTop: "1rem",
-                        bgcolor: selected ? colors.purple[500] : "",
-                        color: selected ? "#d93232ff" : "#000",
-                        "& .MuiListItemText-primary": {
-                          fontWeight: "bold",
-                          color: selected ? "#fff" : "#000",
-                        },
-                        "&:hover": {
-                          bgcolor: colors.purple[300],
-                        },
-                        "&:hover .MuiListItemText-primary": {
-                          color: "#fff",
-                        },
-                        "&.Mui-selected": {
-                          bgcolor: colors.purple[500],
-                          color: "#fff",
-                        },
-                        "&.Mui-selected:hover": {
-                          bgcolor: colors.purple[600],
-                          color: "#fff",
-                        },
-                        "& .MuiListItemIcon-root": {
-                          color: selected ? "#fff" : "#000",
-                        },
+                        justifyContent: isCollapsed ? "center" : "flex-start",
                       }}
                     >
-                      <ListItemIcon sx={{ minWidth: 40 }}>{icon}</ListItemIcon>
-
-                      <ListItemText primary={label} />
-                    </ListItemButton>
-                  </ListItem>
+                      <ListItemButton
+                        selected={selected}
+                        onClick={() => handleNavigate(path)}
+                        sx={{
+                          marginTop: "1rem",
+                          justifyContent: isCollapsed ? "center" : "flex-start",
+                          bgcolor: selected ? colors.purple[500] : "",
+                          color: selected ? "#fff" : "#000",
+                          "&:hover": {
+                            bgcolor: colors.purple[300],
+                            color: "#fff",
+                          },
+                          "&.Mui-selected": {
+                            bgcolor: colors.purple[500],
+                            color: "#fff",
+                          },
+                          "&.Mui-selected:hover": {
+                            bgcolor: colors.purple[600],
+                            color: "#fff",
+                          },
+                          "& .MuiListItemIcon-root": {
+                            color: selected ? "#fff" : "#000",
+                            minWidth: 0,
+                            mr: isCollapsed ? 0 : 2,
+                            justifyContent: "center",
+                          },
+                        }}
+                      >
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        {!isCollapsed && <ListItemText primary={label} />}
+                      </ListItemButton>
+                    </ListItem>
+                  </Tooltip>
                 );
               })}
             </List>
           </Box>
         </Drawer>
+        <Box
+          sx={{
+            position: "fixed",
+            top: "50%",
+            left: isCollapsed ? 60 : 220,
+            transition: "left 0.3s ease",
+            zIndex: 3,
+          }}
+        >
+          <IconButton
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            sx={{
+              background: "#fff",
+              boxShadow: 10,
+              borderRadius: "50%",
+              "&:hover": {
+                backgroundColor: "#f0f0f0",
+              },
+            }}
+          >
+            {isCollapsed ? (
+              <KeyboardDoubleArrowRightIcon />
+            ) : (
+              <KeyboardDoubleArrowLeftIcon />
+            )}
+          </IconButton>
+        </Box>
+
         <Box
           component="main"
           sx={{
@@ -128,10 +171,9 @@ export default function DashBoardLayout({ children }: Props) {
             display: "flex",
             flexDirection: "column",
             minHeight: "100vh",
+            transition: "padding-left 0.5s ease",
           }}
         >
-          <Header />
-
           <Box sx={{ flexGrow: 1, p: 0 }}>{children}</Box>
         </Box>
       </Box>
