036a8fd0b96f5d32fb7bb9eecb604abbeb27ff5f
Ace <ace.gjorgjievski6@gmail.com>
2025-10-22 22:17:44 +0200

    refactor: home page desktop & mobile view

---
+++ train-frontend/src/sections/home/home-mobile-view.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { Box, Typography } from "@mui/material";
+import { useTranslations } from "next-intl";
+import Typewriter from "typewriter-effect";
+
+export default function HomeMobileView() {
+  const tHome = useTranslations("Home");
+
+  return (
+    <>
+      <Box sx={{ position: "relative", zIndex: -1, width: "100%" }}>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          disablePictureInPicture
+          controlsList="nodownload nofullscreen noremoteplayback"
+          style={{
+            width: "105%",
+            height: "97vh",
+            objectFit: "cover",
+            display: "block",
+            marginLeft: "0rem",
+            pointerEvents: "none",
+          }}
+        >
+          <source src="/videos/train_video_home_page.mp4" type="video/mp4" />
+          {tHome("video-additional-message")}
+        </video>
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "#fff",
+            flexDirection: "column",
+            textAlign: "center",
+            px: 2,
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              mt: 5,
+              fontWeight: "bold",
+              fontFamily: "'Anton', sans-serif",
+              fontSize: "1.3rem",
+              color: "#ffffffff",
+              letterSpacing: 2,
+              textShadow: "2px 2px 4px rgba(164, 155, 155, 0.8)",
+              border: "2px solod white",
+              WebkitTextStroke: "0.3px #000000ff",
+            }}
+          >
+            {tHome("screen-title")}
+          </Typography>
+
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: 900,
+              fontFamily: "'Anton', sans-serif",
+              fontSize: "0.7rem",
+              textTransform: "uppercase",
+              color: "#FFD700",
+              letterSpacing: 3,
+              textShadow: "2px 2px 4px rgba(164, 155, 155, 0.8)",
+              WebkitTextStroke: "0.3px #000000ff",
+            }}
+          >
+            {tHome("screen-subtitle")}
+          </Typography>
+
+          <Typography
+            variant="h2"
+            sx={{
+              mb: 2,
+              mt: 7,
+              fontWeight: "bold",
+              fontFamily: "'Anton', sans-serif",
+              fontSize: "2rem",
+              color: "#90ea4fff",
+              letterSpacing: 2,
+              textShadow: "1px 1px 3px rgba(0,0,0,0.4)",
+              WebkitTextStroke: "0.3px #000000ff",
+            }}
+          >
+            <Typewriter
+              options={{
+                strings: [
+                  tHome("screen-subtitle-typewriter-word-one"),
+                  tHome("screen-subtitle-typewriter-word-two"),
+                  tHome("screen-subtitle-typewriter-word-three"),
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 200,
+              }}
+            />
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
